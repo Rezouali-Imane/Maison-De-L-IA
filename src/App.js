d0afd736615fe9dd67a52d65033bae632ee58bb0
unknown <louizadjoudad@gmail.com>
2025-06-26 02:00:29 +0200

    version initiale

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import PixelButton  from './components/PixelButton';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen  flex items-center justify-center bg-black">
+      <h1 className=" text-white text-3xl font-pixel">Tailwind fonctionne !</h1>
+      <PixelButton  className="font-pixel text-white text-xl" onClick={() => alert('Clicked!')}>
+        SIGNUP
+      </PixelButton>
     </div>
   );
 }
